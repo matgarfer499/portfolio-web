0d3e565f7dd61e1baea631f4b9b7e30e5c9454e0
matgarfer499 <matigarfer14@gmail.com>
2025-10-17 23:16:32 +0200

    feat: add new photo assets and update ABOUT_PHOTOS constant with details

---
+++ src/constants/photos.ts
@@ -0,0 +1,76 @@
+import type { Photo } from '@models/photo';
+
+export const ABOUT_PHOTOS: Photo[] = [
+  {
+    src: "/photos/code.webp",
+    alt: "Software made with care",
+    caption: "Technology made with care",
+    rotation: -8,
+    offsetX: -80,
+    offsetY: -20
+  },
+  {
+    src: "/photos/gym.webp",
+    alt: "Gym life",
+    caption: "A lifestyle of strength",
+    rotation: 5,
+    offsetX: 60,
+    offsetY: 30
+  },
+  {
+    src: "/photos/videogames-collage.webp",
+    alt: "Video games",
+    caption: "A collection of my favorite games",
+    rotation: 13,
+    offsetX: 12,
+    offsetY: 50,
+  },
+  {
+    src: "/photos/cheesecake.webp",
+    alt: "Cheesecake",
+    caption: "Food that brings joy",
+    rotation: 12,
+    offsetX: -30,
+    offsetY: 80
+  },
+  {
+    src: "/photos/mygang.webp",
+    alt: "My friends",
+    caption: "Friends who make life better",
+    rotation: -15,
+    offsetX: 90,
+    offsetY: -50
+  },
+  {
+    src: "/photos/furunserland.webp",
+    alt: "Furunserland",
+    caption: "Friends who inspire me to be better",
+    rotation: 10,
+    offsetX: 20,
+    offsetY: 40
+  },
+  {
+    src: "/photos/plane.webp",
+    alt: "Travel adventures",
+    caption: "A never-ending journey",
+    rotation: 3,
+    offsetX: -100,
+    offsetY: 60
+  },
+  {
+    src: "/photos/mylove.webp",
+    alt: "My love",
+    caption: "My soulmate <3",
+    rotation: -12,
+    offsetX: 70,
+    offsetY: -30
+  },
+  {
+    src: "/photos/lamama.webp",
+    alt: "My mother",
+    caption: "The one who raised me",
+    rotation: 0,
+    offsetX: 0,
+    offsetY: 0
+  }
+];
